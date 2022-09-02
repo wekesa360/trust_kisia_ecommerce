004d77e95a5f22153b61d9311c1b7ad6daaa3d80
Michael Wekesa <wekesa360@outlook.com>
2022-09-03 00:12:49 +0300

    add models to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    Product,
+    OrderItem,
+    Order,
+    Category,
+    DeliveryCharges,
+    Customer,
+    ProductImage
+)
 
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Category)
+admin.site.register(DeliveryCharges)
+admin.site.register(Customer)
+
+class PictureInline(admin.StackedInline):
+    model = ProductImage
+    
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PictureInline]
+    
+admin.site.register(Product, ProductAdmin)
