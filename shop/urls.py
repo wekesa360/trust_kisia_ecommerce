291b1158266d1cff7d42218a95f67e3cb839edc7
Michael Wekesa <wekesa360@outlook.com>
2022-09-09 23:32:56 +0300

    upate urls

--- shop/urls.py
+++ shop/urls.py
@@ -6,19 +6,23 @@ from .views import (
     category_products_view,
     category_view,
     checkout_view,
-    order_summary_view
+    order_summary_view,
+    product_view,
+    search_view
 
 )
 
-app_name = 'retail'
+app_name = 'shop'
 
 urlpatterns = [
     path('', category_view, name='home'),
-    path('product<slug>/',category_products_view, name='product'),
+    path('product/<slug>',category_products_view, name='products'),
+    path('product-details/<slug>',product_view, name='product-details'),
     path('order-summary/', order_summary_view, name='order-summary'),
     path('checkout/', checkout_view, name='checkout'),
+    path('search-product', search_view, name='search'),
     path('add-to-cart/<str:slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart'),
-    path('reduce-quantity-item/<slug>/', reduce_quantity_item, name='reduce-quantity-item')
+    path('remove-from-cart/<str:slug>/', remove_from_cart, name='remove-from-cart'),
+    path('reduce-quantity-item/<str:slug>/', reduce_quantity_item, name='reduce-quantity-item')
     
 ]
