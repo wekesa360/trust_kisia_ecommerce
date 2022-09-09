240ca8895fac72da679f78b402a9b90b7dfdfb95
Michael Wekesa <wekesa360@outlook.com>
2022-09-09 23:32:44 +0300

    update forms

--- shop/forms.py
+++ shop/forms.py
@@ -5,23 +5,29 @@ class CheckoutForm(forms.Form):
 
     first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={
         'class': 'form-control',
+        'placeholder': 'first',
+        'required':'required',
         'id': 'first_name',
     }))
     last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={
         'class': 'form-control',
+        'placeholder': 'last',
+        'required':'required',
         'id': 'last_name',
     }))
     email_address = forms.EmailField(required=True, widget=forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': 'john@website.com',
+        'required':'required',
+        'placeholder': 'name@website.com',
         'id': 'email_address',
     }))
     phone_number = forms.CharField(max_length=13, widget=forms.TextInput(attrs={
         'class': 'form-control',
+        'required':'required',
         'placeholder': '+254xxxxxxxxx',
     }))
     delivery_address = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': '1234 Main St',
+        'required':'required',
         'id': 'address'
     }))
