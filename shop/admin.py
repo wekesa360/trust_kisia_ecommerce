a40fef8cc9ac0ac3c3a0af5401356044561da291
Michael Wekesa <wekesa360@outlook.com>
2022-11-29 22:37:46 +0300

    Updated admin dahsboard

--- shop/admin.py
+++ shop/admin.py
@@ -6,19 +6,31 @@ from .models import (
     Category,
     DeliveryCharges,
     Customer,
-    ProductImage
+    ProductImage,
+    ProcessOrder
 )
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Category)
-admin.site.register(DeliveryCharges)
-admin.site.register(Customer)
-
-
-class PictureInline(admin.StackedInline):
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'status', 'ordered_date', 'ordered')
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'ordered', 'customer')
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'sub_category', 'type')
+@admin.register(DeliveryCharges)
+class DeliveryCharges(admin.ModelAdmin):
+    list_display = ('county', 'specific_location', 'fee')
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'email', 'ordered')
+@admin.register(ProcessOrder)
+class ProcessOrderAdmin(admin.ModelAdmin):
+    list_display = ('order', 'total_charges', 'payment_transacted')
+class PictureInline(admin.TabularInline):
     model = ProductImage
-
-
+    
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [PictureInline]
-admin.site.register(Product, ProductAdmin)
+    list_display = ('name', 'quantity', 'brand', 'price', 'tag', 'category')
