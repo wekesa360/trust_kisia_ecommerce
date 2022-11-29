9382c9b5fdff288a694c881f99d3e343ff1d0927
Michael Wekesa <wekesa360@outlook.com>
2022-11-29 22:40:55 +0300

    added email backend

--- trustkisia/settings.py
+++ trustkisia/settings.py
@@ -66,6 +66,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+
 ROOT_URLCONF = 'trustkisia.urls'
 
 TEMPLATES = [
