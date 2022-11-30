2e7f36db1885ea1cfc5e0914d0fc075c24d4084b
Michael Wekesa <wekesa360@outlook.com>
2022-11-30 23:27:21 +0300

    update signals

--- shop/signals.py
+++ shop/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from .models import Order, ProcessOrder, DeliveryCharges, OrderItem
 from django.shortcuts import get_object_or_404
+from .functions import send_confirm_order_email
 from django.dispatch import receiver
 import logging
 
@@ -8,15 +9,14 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=ProcessOrder)
 def handle_new_order(sender, instance, created, **kwargs):
-    print('signal>>>>>>>>>>>>>>>',created)
-    print(kwargs.get('updated'), '<<<<<<<<<<<<<<<<signal')
     if created:
         order = ProcessOrder.objects.get(id=instance.id)
         if order.cancel_order == False:
-            order_items = OrderItem.objects.filter(customer=order.order.customer)
-            first_name = order.order.customer.first_name
-            order_id = order.order.customer.first_name
-            print(order.order.uuid, order_items)
+            send_confirm_order_email(order)
+        else:
+            print(order.order.uuid + "  :order cancelled")
+
+            
 
 
  
