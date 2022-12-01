040a75e9cc7e9d5b94a5b18e5d404f3758e05048
Michael Wekesa <wekesa360@outlook.com>
2022-12-01 18:11:30 +0300

    update: fix processorder, add cancel order signals

--- shop/signals.py
+++ shop/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
-from .models import Order, ProcessOrder, DeliveryCharges, OrderItem
+import uuid
+from .models import Customer, ProcessOrder, CancelledOrder
 from django.shortcuts import get_object_or_404
-from .functions import send_confirm_order_email
+from .functions import send_confirm_order_email, send_cancel_order_email, get_total_charges
 from django.dispatch import receiver
 import logging
 
@@ -13,10 +14,20 @@ def handle_new_order(sender, instance, created, **kwargs):
         order = ProcessOrder.objects.get(id=instance.id)
         if order.cancel_order == False:
             send_confirm_order_email(order)
+            order.total_charges = get_total_charges(order)[1]
+            order.save()
         else:
             print(order.order.uuid + "  :order cancelled")
 
-            
+@receiver(post_save, sender=CancelledOrder)
+def handle_new_order(sender, instance, created, **kwargs):
+    if created:
+        cancelled_order = CancelledOrder.objects.get(id=instance.id)
+        if cancelled_order.order_cancelled == True:
+            send_cancel_order_email(cancelled_order)
+        else:
+            print(cancelled_order.order_id + "  :order not cancelled")
+        
 
 
  
