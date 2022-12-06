871ed6fd63d812094be9391c5735d80ecf466b56
Michael Wekesa <wekesa360@outlook.com>
2022-12-06 15:34:00 +0300

    update: exclude slug field

--- shop/admin.py
+++ shop/admin.py
@@ -9,38 +9,52 @@ from .models import (
     ProductImage,
     ProcessOrder,
     CancelledOrder,
-    EmailDispatch
+    EmailDispatch,
+    SubCategory
 )
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('customer', 'status', 'ordered_date', 'ordered')
+    exclude = ('slug',)
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('product', 'quantity', 'ordered', 'customer')
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    exclude = ('slug',)
+    
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category_name', 'sub_category', 'type')
+    inlines = [SubCategoryInline]
+    exclude = ('slug',)
+
 @admin.register(DeliveryCharges)
 class DeliveryCharges(admin.ModelAdmin):
     list_display = ('county', 'specific_location', 'fee')
+    exclude = ('slug',)
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('first_name','device', 'phone_number', 'email', 'ordered')
+    exclude = ('slug',)
 @admin.register(ProcessOrder)
 class ProcessOrderAdmin(admin.ModelAdmin):
     list_display = ('order', 'total_charges', 'payment_transacted', 'cancel_order')
+    exclude = ('slug',)
 class PictureInline(admin.TabularInline):
     model = ProductImage
     
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [PictureInline]
-    list_display = ('name', 'quantity', 'brand', 'price', 'tag', 'category')
+    list_display = ('name', 'quantity', 'brand', 'price', 'tag',)
+    exclude = ('slug',)
 
 @admin.register(CancelledOrder)
 class CancelledOrderAdmin(admin.ModelAdmin):
     list_display = ('customer_name', 'customer_email', 'order_id', 'products_order', 'order_cancelled')
+    exclude = ('slug',)
 
 @admin.register(EmailDispatch)
 class EmailDispatchAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject')
+    exclude = ('slug',)
